7082acb118ef8f6bbedfe7a48cabe53c18fbfe73
vjeantet <valere.jeantet@gmail.com>
2026-03-04 23:13:11 +0100

    Update alpaca-proxy to 26.2

--- Formula/alpaca-proxy.rb
+++ Formula/alpaca-proxy.rb
@@ -1,23 +1,30 @@
 class AlpacaProxy < Formula
   desc "Local HTTP proxy with PAC, NTLM, Basic and Kerberos authentication"
   homepage "https://github.com/vjeantet/alpaca"
-  version "26.1"
+  version "26.2"
   license "Apache-2.0"
   depends_on :macos
 
   on_arm do
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.1/alpaca_v26.1_darwin-arm64"
-    sha256 "a6425b4db9e35fb53cd8779ccf4b9303975d9d5796220e7910a49f5c931ab009"
+    url "https://github.com/vjeantet/alpaca/releases/download/v26.2/alpaca_v26.2_darwin-arm64"
+    sha256 "097e33b1d3a9cc5e26d86f9431c756eb39d7a3d1fe38b1079c2114222b8be1ed"
   end
 
   on_intel do
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.1/alpaca_v26.1_darwin-amd64"
-    sha256 "48f784f0caeba8bbbe523acd7fcbd88a1722f3a2412f65f8218ff24775de3126"
+    url "https://github.com/vjeantet/alpaca/releases/download/v26.2/alpaca_v26.2_darwin-amd64"
+    sha256 "8116bf1fd6b28ea2db80253a453feb0833202db3dc999b84a955b244554c85ac"
   end
 
   def install
     binary_name = stable.url.split("/").last
     bin.install binary_name => "alpaca"
+  end
+
+  service do
+    run [opt_bin/"alpaca"]
+    keep_alive true
+    log_path var/"log/alpaca-proxy.log"
+    error_log_path var/"log/alpaca-proxy.log"
   end
 
   test do
