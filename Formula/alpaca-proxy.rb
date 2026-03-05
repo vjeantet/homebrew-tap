c2579c2c56b4756025f21ae48fdd29cc9b6d35d8
vjeantet <valere.jeantet@gmail.com>
2026-03-05 17:23:28 +0100

    Update alpaca-proxy to 26.5

--- Formula/alpaca-proxy.rb
+++ Formula/alpaca-proxy.rb
@@ -1,18 +1,18 @@
 class AlpacaProxy < Formula
   desc "Local HTTP proxy with PAC, NTLM, Basic and Kerberos authentication"
   homepage "https://github.com/vjeantet/alpaca"
-  version "26.4"
+  version "26.5"
   license "Apache-2.0"
   depends_on :macos
 
   on_arm do
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.4/alpaca_v26.4_darwin-arm64"
-    sha256 "1a36dc066b650e9ef7f17580b65e2036fc219de7779d2aba44eb45c360e4efc2"
+    url "https://github.com/vjeantet/alpaca/releases/download/v26.5/alpaca_v26.5_darwin-arm64"
+    sha256 "34f9110cf5f71e18614d746f5fed29541b349e64dda917eb6d8187c3b80e9da4"
   end
 
   on_intel do
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.4/alpaca_v26.4_darwin-amd64"
-    sha256 "b1e9c9f5ea7a21606c597134ea913afd13a72f57405594912d6ae1c8d199eceb"
+    url "https://github.com/vjeantet/alpaca/releases/download/v26.5/alpaca_v26.5_darwin-amd64"
+    sha256 "273b3e1c36aaaeeb6647d5ced6bc95b4f6b3fccc06f64d5dafb612d6f2804855"
   end
 
   def install
@@ -20,53 +20,9 @@ class AlpacaProxy < Formula
     bin.install binary_name => "alpaca"
   end
 
-  def post_install
-    config_dir = Pathname.new("#{Dir.home}/.config/alpaca")
-    config_file = config_dir/"config.yaml"
-    return if config_file.exist?
-
-    config_dir.mkpath
-    config_file.write <<~YAML
-      # Alpaca proxy configuration
-      # See https://github.com/vjeantet/alpaca for documentation.
-      #
-      # CLI flags always override values set here.
-      # "password" and "basic-credentials" are not allowed in this file
-      # for security reasons.
-
-      # Address(es) to listen on (default: localhost)
-      # listen:
-      #   - localhost
-
-      # Port to listen on (default: 8079)
-      # port: 8079
-
-      # URL of the proxy auto-config (PAC) file
-      # pac-url: ""
-
-      # Domain for NTLM proxy authentication
-      # domain: ""
-
-      # Username for NTLM proxy authentication (default: current user)
-      # username: ""
-
-      # Enable Kerberos/Negotiate proxy authentication (macOS only)
-      # kerberos: false
-
-      # Seconds to wait for a Kerberos ticket (macOS only, default: 30)
-      # kerberos-wait: 30
-
-      # Suppress all log output
-      # quiet: false
-
-      # Emit JSON log lines on stdout
-      # json-logs: false
-    YAML
-  end
-
   def caveats
     <<~EOS
-      A default configuration file has been created at:
+      On first launch, a default configuration file will be created at:
         ~/.config/alpaca/config.yaml
 
       Edit it to set your proxy settings, then start the service with:
@@ -76,9 +32,9 @@ class AlpacaProxy < Formula
         export http_proxy=http://localhost:8079
         export https_proxy=http://localhost:8079
 
-      The logs are in :
+      The logs are in:
         /opt/homebrew/var/log/alpaca-proxy.log
-        
+
     EOS
   end
 
