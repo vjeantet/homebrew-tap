6ee534e207d3b50c5cd8ec92183cd9a5106a3626
vjeantet <valere.jeantet@gmail.com>
2026-03-09 15:51:33 +0100

    Update alpaca-proxy to 26.7

--- Formula/alpaca-proxy.rb
+++ Formula/alpaca-proxy.rb
@@ -1,18 +1,28 @@
 class AlpacaProxy < Formula
   desc "Local HTTP proxy with PAC, NTLM, Basic and Kerberos authentication"
   homepage "https://github.com/vjeantet/alpaca"
-  version "26.6"
+  version "26.7"
   license "Apache-2.0"
   depends_on :macos
 
   on_arm do
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.6/alpaca_v26.6_darwin-arm64"
-    sha256 "a7fcf4d1d4a6206682a29a8194c024ce07b15feb44b96cc282ab2ce095d5d5e9"
+    if ENV["ALPACA_DEV"]
+      url "https://github.com/vjeantet/alpaca/releases/download/v26.7/alpaca_v26.7-dev_darwin-arm64"
+      sha256 "75f38ed68d6adbf531a897ab868ce99e336bb77cb4b0b0d653782291fa5b83e9"
+    else
+      url "https://github.com/vjeantet/alpaca/releases/download/v26.7/alpaca_v26.7_darwin-arm64"
+      sha256 "a36e30f5e4d8abe816932ba0454972542f1472053ce9d543a35dbd493f086793"
+    end
   end
 
   on_intel do
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.6/alpaca_v26.6_darwin-amd64"
-    sha256 "21e73de8455c5bbeab08bb550a895fef04867d9c16c7fe0fe829aa9efb454330"
+    if ENV["ALPACA_DEV"]
+      url "https://github.com/vjeantet/alpaca/releases/download/v26.7/alpaca_v26.7-dev_darwin-amd64"
+      sha256 "8faedf738f71fb76d54e081d08665077e9ebf2c681fae85ddb7bbc7142fbc0f0"
+    else
+      url "https://github.com/vjeantet/alpaca/releases/download/v26.7/alpaca_v26.7_darwin-amd64"
+      sha256 "bcbcd2cf1b496c5349d8aff61469e97f0862e1903b283624850d1f7191426280"
+    end
   end
 
   def install
