353216fc052e1d6657f835142f69b0d2a0be81f4
vjeantet <valere.jeantet@gmail.com>
2026-03-25 22:45:20 +0100

    Update alpaca-proxy to 26.11

--- Formula/alpaca-proxy.rb
+++ Formula/alpaca-proxy.rb
@@ -1,16 +1,16 @@
 class AlpacaProxy < Formula
   desc "Local HTTP proxy with PAC, NTLM, Basic and Kerberos authentication"
   homepage "https://github.com/vjeantet/alpaca"
-  version "26.10"
+  version "26.11"
   license "Apache-2.0"
   depends_on :macos
 
   if ENV["ALPACA_DEV"]
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.10/alpaca_v26.10-dev_darwin-arm64"
-    sha256 "7701176187c4f02883a90f6858ed518d679c231d8c9525394336822f2929fc0b"
+    url "https://github.com/vjeantet/alpaca/releases/download/v26.11/alpaca_v26.11-dev_darwin-arm64"
+    sha256 "561e31a1f5235b023ffce906a03f1bf922df85bfa51a9eaa40be8a2cc8ffcb92"
   else
-    url "https://github.com/vjeantet/alpaca/releases/download/v26.10/alpaca_v26.10_darwin-arm64"
-    sha256 "1c9646acf929f1d86a8f132c3f00e24bc69bfe2c6bf7f4a7198252efd18b9578"
+    url "https://github.com/vjeantet/alpaca/releases/download/v26.11/alpaca_v26.11_darwin-arm64"
+    sha256 "cb5d01c6c6cde2bfc8dad8d39f93dd212feadb52229142661c619a2150b6b89f"
   end
 
   def install
