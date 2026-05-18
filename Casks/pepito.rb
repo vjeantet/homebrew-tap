7512837755eb07faa9f7849d0d6aece0eb9aea45
vjeantet <valere.jeantet@gmail.com>
2026-05-18 22:01:52 +0200

    Update pepito to 26.28

--- Casks/pepito.rb
+++ Casks/pepito.rb
@@ -1,6 +1,6 @@
 cask "pepito" do
-  version "26.25"
-  sha256 "cad2700187ba53cb72f859c48aabb77ea62603237e09b9f1db9ff06034b19262"
+  version "26.28"
+  sha256 "e42a1076fea5f885c864a636a9b3c4fe26c7490f4252f43c7ac29c2b84e648fd"
 
   url "https://github.com/vjeantet/aipepito-releases/releases/download/v#{version}/pepito.dmg"
   name "pepito"
